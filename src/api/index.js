ee3a3d5628f721c27fb7c08487c54d2fac7d4c1c
GlistenSTAR <glistenstar00@gmail.com>
2022-04-28 09:49:05 +0200

    add the store and migrate the api index

--- src/api/index.js
+++ src/api/index.js
@@ -11,6 +11,9 @@ export function getMarkets(count = 250) {
   return getMarketsRecursive(1, Math.min(count, 250), count)
 }
 
+/**
+ * @param {any} ids
+ */
 export function getMarketsByIds(ids) {
   return axios.get(`${coingeckoBaseUrl}/markets?vs_currency=USD&order=market_cap_desc&sparkline=false&price_change_percentage=24h,7d,14d,30d,200d,1y&ids=${ids}`)
 }
@@ -19,10 +22,16 @@ export function getDefiCoins() {
   return axios.get(`${marketsBaseUrl}/markets/defi?diff_period=24h,7d,30d`)
 }
 
+/**
+ * @param {any} coin
+ */
 export function getCoinTvlChart(coin, period = '7d') {
   return axios.get(`${marketsBaseUrl}/markets/defi/${coin}/${period}`)
 }
 
+/**
+ * @param {any} coin
+ */
 export function getCoinVolumeChart(coin) {
   return axios.get(`${coingeckoBaseUrl}/${coin}/market_chart?vs_currency=USD&days=1`)
 }
@@ -31,6 +40,9 @@ export function getDefiMarkets() {
   return axios.get(`${coingeckoBaseUrl}/markets?vs_currency=USD&order=market_cap_desc&sparkline=false&price_change_percentage=24h,7d,14d,30d,200d,1y&category=decentralized_finance_defi`)
 }
 
+/**
+ * @param {any} id
+ */
 export function getCoinInfo(id) {
   return axios.get(`${coingeckoBaseUrl}/${id}?localization=false&tickers=true&&sparkline=true`)
 }
@@ -39,6 +51,7 @@ export function getNews() {
   return axios.get('https://min-api.cryptocompare.com/data/v2/news/?feeds=cointelegraph,theblock,decrypt&extraParams=Blocksdecoded&excludeCategories=Sponsored')
 }
 
+// @ts-ignore
 const getMarketsRecursive = async (page = 1, perPage = 250, count = 250) => {
   const { data } = await axios.get(`${coingeckoBaseUrl}/markets?vs_currency=USD&order=market_cap_desc&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=24h,7d,14d,30d,200d,1y`)
 
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { writable } from 'svelte/store';
+
+export const coinInfo = writable({});
+export const discovery = writable({});
+export const markets = writable({});
+export const defi = writable({});
+export const marketsGlobal = writable({});
+export const news = writable({});
+export const watchlist = writable({});
