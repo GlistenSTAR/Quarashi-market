f2432ee5d49f26222aafd28df3e07bf4b1ef1917
GlistenSTAR <glistenstar00@gmail.com>
2022-04-29 08:55:53 +0200

    getMarkets, getMarkets, getDefi api added.

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import API from './../utils/api'
 
-const coingeckoBaseUrl = 'https://api.coingecko.com/api/v3/coins'
+const marketsBaseUrl = 'markets.horizontalsystems.xyz/api/v1'
+const coingeckoBaseUrl = 'api.coingecko.com/api/v3/coins'
 
 import {
   coinInfo,
@@ -11,65 +12,21 @@ import {
   news,
 } from "./../store";
 
-function normalize(data) {
-  if (!data || !data.length) {
-    return {
-      markets: [],
-      points: {}
-    }
-  }
+import { normalize, normalizeCoins, normalizeDefiCoins } from './filters';
 
-  const latest = data[data.length - 1]
-  const data24 = data[0]
-  const sorted = data.sort((a, b) => a.timestamp - b.timestamp)
-  const change = (from, to) => (from - to) * 100 / to
-  const points = {
-    volume: [],
-    dominance: [],
-    marketCap: [],
-    defiMarket: [],
-    tvl: []
-  }
-
-  for (let i = 0; i < sorted.length; i++) {
-    const item = sorted[i]
-    const time = parseInt(item.timestamp)
-
-    points.volume.push({ time, value: item.volume24h })
-    points.dominance.push({ time, value: item.dominance_btc })
-    points.marketCap.push({ time, value: item.market_cap })
-    points.defiMarket.push({ time, value: item.market_cap_defi })
-    points.tvl.push({ time, value: item.tvl })
-  }
-
-  return {
-    points,
-    marketCap: latest.market_cap,
-    marketCapDefi: latest.market_cap_defi,
-    volume24h: latest.volume24h,
-    dominanceBTC: latest.dominance_btc,
-    totalValueLocked: latest.tvl,
-    marketCapDiff24h: change(latest.market_cap, data24.market_cap),
-    marketCapDefiDiff24h: change(latest.market_cap_defi, data24.market_cap_defi),
-    dominanceBTCDiff24h: change(latest.dominance_btc, data24.dominance_btc),
-    volume24hDiff: change(latest.volume24h, data24.volume24h),
-    totalValueLockedDiff24h: change(latest.tvl, data24.tvl)
-  }
-}
-
-export const getMarketsGlobal = async () => {
+export async function getMarketsGlobal() {
   try {
-    const response = await API.get("/markets/global/24h");
-    console.log(normalize(response))
+    const response = await API.get(marketsBaseUrl + "/markets/global/24h");
     marketsGlobal.set(normalize(response));
     return response
   } catch (error) {
     console.error(error);
   }
-};
+}
 
-export function getMarkets(count = 250) {
-  return getMarketsRecursive(1, Math.min(count, 250), count)
+export async function getMarkets(count = 250) {
+  const data = markets.set(normalizeCoins(await getMarketsRecursive(1, Math.min(count, 250), count)))
+  return data
 }
 
 /**
@@ -79,8 +36,12 @@ export function getMarketsByIds(ids) {
   return API.get(`${coingeckoBaseUrl}/markets?vs_currency=USD&order=market_cap_desc&sparkline=false&price_change_percentage=24h,7d,14d,30d,200d,1y&ids=${ids}`)
 }
 
-export function getDefiCoins() {
-  return API.get(`/markets/defi?diff_period=24h,7d,30d`)
+export async function getMarkets() {
+  const data = await API.get(`${marketsBaseUrl}/markets/defi?diff_period=24h,7d,30d`)
+  const result = normalizeDefiCoins(data);
+  defi.set(result);
+  // console.log('>>>>>', defi)
+  return data
 }
 
 /**
@@ -114,12 +75,15 @@ export function getNews() {
 
 // @ts-ignore
 const getMarketsRecursive = async (page = 1, perPage = 250, count = 250) => {
-  const { data } = await API.get(`${coingeckoBaseUrl}/markets?vs_currency=USD&order=market_cap_desc&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=24h,7d,14d,30d,200d,1y`)
+  try {
+    const data = await API.get(coingeckoBaseUrl + `/markets?vs_currency=USD&order=market_cap_desc&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=24h,7d,14d,30d,200d,1y`)
+    if (data.length < perPage || data.length >= count) {
+      return data
+    }
+    return data.concat(await getMarketsRecursive(page + 1, count))
 
-  if (data.length < perPage || data.length >= count) {
-    return data
+  } catch (error) {
+    console.error(error);
   }
-
-  return data.concat(await getMarketsRecursive(page + 1, count))
 }
 
