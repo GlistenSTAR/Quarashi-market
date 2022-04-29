6c2ac909e7849f5d0cd08b4d557bd3f6b3083932
GlistenSTAR <glistenstar00@gmail.com>
2022-04-29 08:56:28 +0200

    fix the uniquekey erros in loadervalue

--- src/lib/components/loader/LoaderValue.svelte
+++ src/lib/components/loader/LoaderValue.svelte
@@ -1,12 +1,12 @@
 <script>
-    import ContentLoader from 'svelte-content-loader';
-
-    export const width=""
-    export const height=""
+    import ContentLoader from "svelte-content-loader";
+    export let width = "";
+    export let height = "";
+    export let key = "";
 </script>
 
 <span>
-    <ContentLoader>
-        <rect x="0" y="0" rx="3" ry="3" width={width} height={height} />
+    <ContentLoader uniqueKey={key} >
+        <rect x="0" y="0" rx="3" ry="3" {width} {height}/>
     </ContentLoader>
 </span>
