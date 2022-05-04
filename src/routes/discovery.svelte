b8fc21e0110138b050bb9f5683e99d147e628e55
GlistenSTAR <glistenstar00@gmail.com>
2022-05-04 15:38:13 +0200

    change dir name

--- src/routes/discovery.svelte
+++ src/routes/discovery.svelte
@@ -1,31 +1,18 @@
 <script>
     // @ts-nocheck
-    import { onMount } from "svelte";
-
-    import CoinBox from "$lib/components/CoinBox.svelte";
-    import CoinTable from "$lib/components/coin_table/Coin-Table.svelte";
+    
     import Loading from "$lib/components/loader/Loading.svelte";
-    import AdvancedSearch from "$lib/components/advacnedSearch/AdvancedSearch.svelte";
+    import AdvancedSearch from "$lib/components/Search/AdvancedSearch.svelte";
+    import CategorySearch from "$lib/components/Search/CategorySearch.svelte";
 
     import isEmpty from "./../utils/is-empty";
-    import coinStore from "$lib/coins-store";
-    import { markets } from "./../store";
+    import { markets, temp} from "./../store";
 
     let active = "cat";
-    let watchlist = false;
-    /**
-     * @type {typeof import("svelte-carousel").default}
-     */
-    let Carousel, carousel;
 
-    onMount(async () => {
-        const module = await import("svelte-carousel");
-        Carousel = module.default;
-    });
-
-    const setActive = (/** @type {string} */ val) => {
+    const setActive = (val) => {
         active = val;
-    };
+    };    
 </script>
 
 <svelte:head>
@@ -57,25 +44,10 @@
         </ul>
 
         {#if active === "cat"}
-            <div class="card-carousel">
-                <svelte:component
-                    this={Carousel}
-                    bind:this={carousel}
-                    particlesToShow={4}
-                >
-                    {#each coinStore.categories as item}
-                        <CoinBox
-                            name={item.name}
-                            content={item.description}
-                            icon={item.id}
-                        />
-                    {/each}
-                </svelte:component>
-            </div>
-            <CoinTable {watchlist} />
+            <CategorySearch />
         {/if}
         {#if active === "adv"}
-            <AdvancedSearch {watchlist} />
+            <AdvancedSearch />
         {/if}
     {/if}
 </section>
@@ -112,16 +84,4 @@
         border-bottom: 2px solid #c921cd;
     }
 
-    .card-carousel {
-        margin-top: 40px;
-        margin-left: -50px;
-        margin-right: -50px;
-    }
-
-    @media (max-width: 1352px) {
-        .card-carousel {
-            margin-left: 0px;
-            margin-right: 0px;
-        }
-    }
 </style>
