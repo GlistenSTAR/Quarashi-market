cd635ed7969ffff746cae684ff1bb7fb92ff7a7f
GlistenSTAR <glistenstar00@gmail.com>
2022-07-15 20:21:37 +0200

    add datatable with svelte-simple-datetables

--- src/lib/components/coin_table/Coin-Table.svelte
+++ src/lib/components/coin_table/Coin-Table.svelte
@@ -3,7 +3,7 @@
     import { page } from "$app/stores";
     import Select from "svelte-select";
     import { total_items } from "../../selectData";
-    import Datatable from "./Datatable.svelte";
+    import { Datatable, rows } from "svelte-simple-datatables";
     import isEmpty from "./../../../utils/is-empty";
     import { StarIcon } from "svelte-feather-icons";
     import {
@@ -86,6 +86,63 @@
             }
         }
     };
+
+    const settings = {
+        pagination: true,
+        rowsPerPage: 50,
+        columnFilter: false,
+        searchInput: false,
+        scrollY: false,
+        labels: {
+            noRows: "No entries to found",
+            previous: "<",
+            next: ">",
+        },
+        blocks: {
+            searchInput: false,
+            paginationRowCount: false,
+        },
+    };
+
+    import {
+        volume,
+        percentageFormat,
+        priceColor,
+        currencyFullValue,
+    } from "./../../../helpers";
+
+    let oldWatchlist = JSON.parse(localStorage.getItem("watchlist"));
+
+    const data1 = $categoriesData.slice(0, 70);
+
+    const goCoindetail = (id) => {
+        window.location = "/coins?id=" + id;
+    };
+
+    const setFav = (id, event) => {
+        // if (!isEmpty(oldWatchlist)) {
+        //     if (oldWatchlist[`${id}`] === true) {
+        //         event.target.classList.remove("active");
+        //         delete oldWatchlist[`${id}`];
+        //     } else {
+        //         event.target.classList.add("active");
+        //         oldWatchlist[`${id}`] = true;
+        //     }
+        // } else {
+        //     oldWatchlist = {};
+        //     oldWatchlist[`${id}`] = true;
+        // }
+        // let changedData = [];
+        // $markets.map((item) => {
+        //     if (oldWatchlist[`${item.id}`]) {
+        //         changedData.push(item);
+        //     }
+        // });
+        // localStorage.setItem("watchlist", JSON.stringify(oldWatchlist));
+        // watchlistData.set(changedData);
+        // watchlist.set(oldWatchlist);
+    };
+    $: console.log(data);
 </script>
 
 <div class="coin-table">
@@ -105,7 +162,7 @@
         </div>
     {:else}
         <div>
-            <div class="coin_kind col-md-4">
+            <div class="coin_kind col-md-4 mb-5">
                 <Select
                     items={total_items}
                     value={selected}
@@ -115,7 +172,73 @@
             </div>
         </div>
         <div class="table-res">
-            <Datatable {method} />
+            <Datatable {settings} data={data1} class="coin-table">
+                <thead>
+                    <th class="text-center" width="5%">Watchlist</th>
+                    <th class="text-center" data-key="rank" width="7%">#</th>
+                    <th data-key="name" width="25%">Name</th>
+                    <th data-key="price">Price</th>
+                    <th data-key="priceChange24h" class="text-center"
+                        >24 Hours</th
+                    >
+                    <th data-key="priceChange7d" class="text-center" width="10%"
+                        >7 days</th
+                    >
+                    <th data-key="marketCap">Market Cap</th>
+                    <th data-key="totalVolume" class="right">Volume</th>
+                </thead>
+                <tbody>
+                    {#each $rows as row}
+                        <tr>
+                            <td
+                                class="text-center"
+                                on:click|preventDefault={(event) =>
+                                    setFav(row.id, event)}
+                            >
+                                {#if oldWatchlist && oldWatchlist[`${row.id}`]}
+                                    <StarIcon class="active" size="16" />
+                                {:else}
+                                    <StarIcon size="16" />
+                                {/if}
+                            </td>
+                            <td class="text-center"
+                                >{row.rank ? row.rank : ""}</td
+                            >
+                            <td
+                                on:click={goCoindetail(row.id)}
+                                style="cursor: pointer;"
+                                ><img
+                                    src={row.image}
+                                    alt="coin"
+                                    width="24"
+                                    height="24"
+                                /><span class="name_padding">{row.name}</span
+                                ></td
+                            >
+                            <td>{currencyFullValue(row.price)}</td>
+                            <td
+                                class="{priceColor(
+                                    row.priceChange24h
+                                )} text-center"
+                                >{row.priceChange24h
+                                    ? percentageFormat(row.priceChange24h)
+                                    : 0}</td
+                            >
+                            <td
+                                class="{priceColor(
+                                    row.priceChange7d
+                                )} text-center"
+                                >{row.priceChange7d
+                                    ? percentageFormat(row.priceChange7d)
+                                    : 0}</td
+                            >
+                            <td>$ {volume(row.marketCap)}</td>
+                            <td class="right">$ {volume(row.totalVolume)}</td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </Datatable>
+            <!-- <Datatable {method} /> -->
         </div>
     {/if}
 </div>
@@ -141,7 +264,7 @@
         background-color: #141421;
         border-radius: 12px;
         padding: 20px;
-        padding-bottom: 60px;
+        padding-bottom: 40px;
     }
     .empty_table {
         margin-top: 40px;
@@ -155,5 +278,32 @@
         .table-res {
             overflow-x: auto;
         }
+    }
+
+    thead {
+        border-radius: 1px solid grey;
+    }
+    th {
+        text-align: left;
+        font-size: 12px;
+        color: grey;
+        cursor: pointer;
+    }
+    td {
+        text-align: left;
+        padding: 4px 0;
+    }
+
+    th.right,
+    td.right {
+        text-align: right;
+    }
+
+    td.text-center {
+        text-align: right;
+    }
+
+    .name_padding {
+        margin-left: 10px;
     }
 </style>
